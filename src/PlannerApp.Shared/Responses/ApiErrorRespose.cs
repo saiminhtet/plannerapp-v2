bc90d009bc0239d4009337402e144f7b8e3d5756
Sai Min Htet <saiminhtet@u.nus.edu>
2021-07-21 16:52:06 +0800

    Create the API response models and init the HttpClient instance

---
+++ src/PlannerApp.Shared/Responses/ApiErrorRespose.cs
@@ -0,0 +1,10 @@
+﻿namespace PlannerApp.Shared.Responses
+{
+    public class ApiErrorRespose
+    {
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+
+}
---
+++ src/PlannerApp/Program.cs
@@ -0,0 +1,37 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("PlannerApp.Api", client =>
+            {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+            builder.Services.AddTransient<AuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+
+            builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
