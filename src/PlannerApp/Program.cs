95f5b46ae8f5dc902eec0f3153d71847c17f02f1
Sai Min Htet <saiminhtet@u.nus.edu>
2021-07-23 12:24:16 +0800

    Added authentication component

--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,12 +25,14 @@ namespace PlannerApp
             {
                 client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
             }).AddHttpMessageHandler<AuthorizationMessageHandler>();
-
             builder.Services.AddTransient<AuthorizationMessageHandler>();
+
             builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
 
             builder.Services.AddMudServices();
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
 
             await builder.Build().RunAsync();
         }
